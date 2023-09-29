2842edf3532533d60e8c48b1e652fbcf69473b40
purnachandrasarma1 <68757568+purnachandrasarma1@users.noreply.github.com>
2023-09-29 01:43:40 -0400

    #Refactor AWS CLI into a bash script (#9)

--- bin/install-terraform-cli.sh
+++ bin/install-terraform-cli.sh
@@ -1,6 +1,4 @@
 #!/usr/bi/env bash
-
-PROJECT_ROOT '/workspace/terraform-beginner-bootcamp-2023'
 cd /workspace
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 wget -O- https://apt.releases.hashicorp.com/gpg | \
