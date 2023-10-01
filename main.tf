d204b3648887184f3f71ae4b692a453cfd03b904
purnachandrasarma1 <68757568+purnachandrasarma1@users.noreply.github.com>
2023-09-30 23:18:50 -0400

    #10 Add Rrandom tterraform Provider (#11)

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
-This is my first change.
-This is my second change in Terraform.
-This is my third change from GitHub portal.
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "random_bucket_name_id" {
+  length = 8
+  special = false
+}
+
+resource "random_string" "random_bucket_name" {
+  length = 12
+  special = false
+}
+
+
+output "random_bucket_name_result" {
+    value = random_string.random_bucket_name.result
+}
+
